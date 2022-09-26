0f42726404435516f14b6380e7a892670a0a9aef
Rey Gonzalez <gonza736@purdue.edu>
2022-09-26 10:00:23 -0400

    cleaned NDRAM added CUDA_NDRAM

--- NDRAM.py
+++ NDRAM.py
@@ -13,10 +13,7 @@ import time
 ## h <- learning parameter
 # output: updated weight matrix <- W
 def ndram_learn(W, x0, xt, h):
-    left = np.outer(x0, x0)
-    right = np.outer(xt, xt)
-
-    return np.array(W + h * np.array(left - right))
+    return np.array(W + h * (np.outer(x0, x0) - np.outer(xt, xt)))
 
 # convergence equation (convergence: _lambda -> 1; d_delta -> 0)
 # inputs:
@@ -105,10 +102,10 @@ def loop_init():
     return 0, 1, 1, time.time()
 
 # test - 28 signals with subsequent 1s
-side = 28
+side = 128
 blank = [-1] * side
 blank = np.array(blank)
-stimuli = [blank.copy() for i in range(28)]
+stimuli = [blank.copy() for i in range(int(side/3))]
 
 for i, x0 in enumerate(stimuli):
     x0[i] = 1
@@ -119,18 +116,18 @@ W = initial_weights(side)
 # learning parameter (suggested 0.001 - 0.002)
 h = 0.001
 # transmission parameter (suggested 0.1 - 0.5)
-delta = 0.1
+delta = 0.5
 
 # prepare data
 _lambda, d_lambda, counter, timer = loop_init()
 
-# easy to loop around _lambda
-while _lambda < 0.999:
+# easy to loop around _lambda (converges to 1)
+while _lambda < 0.9999:
     counter = readout_and_counter(timer, counter, _lambda, d_lambda)
     
-    # for each stimuli, transmit n times, then learn
+    # for each stimuli, transmit n times, then update weight matrix
     for x0 in stimuli:
-        W = transmit_and_learn(W, x0, delta, h, 3)
+        W = transmit_and_learn(W, x0, delta, h, 7)
 
     # update convergence values
     _lambda, d_lambda = convergence(W, h, delta)
